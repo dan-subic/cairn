f4d7df7a827a2957c0b468063a7eeebc73ac3b4b
Daniel Subic <daniel.subic@eborsystems.com>
2022-03-04 23:38:17 +1030

    Done gifts!

--- character-generator/js/main.js
+++ character-generator/js/main.js
@@ -2,7 +2,14 @@ roll = (d) => {
   return Math.floor(Math.random()*d)+1;
 };
 
-const gifts = []
+const gifts = [
+  ['Saliva is a powerful acid', 	'Invisible while squeezing eyes shut', 	'Can manipulate wood with hands as if it were clay', 	'Can transform into a horse', 	'Can bite through any material, and safely eat anything', 	'Communicate with any creature'],
+  ['30ft prehensile tongue', 	'Perfect throwing accuracy, except when targeting a living creature', 	'Can dislodge own soul to look into the spirit world by taking 1 Fatigue', 	'Can secretly steal small objects with a mere touch', 	'Flammable touch & heat-resistant', 	'Excellent swimming & don’t need to breathe'],
+  ['Body parts can be detached and reattached, can control detached parts at range', 	'When squeezing one eye shut, can see in the dark, but not in the light', 	'Briefly very strong after taking strong alcohol', 	'Extra arm', 	'Climb any surface while hands & feet are bare', 	'Perfectly mimic someone’s voice or replicate any sound heard before'],
+  ['Can change someone’s face at a touch. Reverts when washed', 	'Can hock up a very sticky ball of gunk', 	'Gale-force sneeze', 	'Can leap a long distance, fall from any height unharmed', 	'Voice can come from somewhere nearby', 	'Slippery skin while holding breath'],
+  ['Long, indestructible hair', 	'Bloodhound-scent while eyes are closed', 	'Freezing touch & cold-resistant', 	'Safely swallow any item and regurgitate it later', 	'Can dig a hole or tunnel very fast', 	'Can lay tasty green eggs'],
+  ['Can summon one starting item at will', 	'Shrink self & gear to 10% of original size', 	'Perfect balance', 	'Echolocate by singing', 	'Can snap fingers loud enough to deafen', 	'Thrown coins & dice always land on the desired side']
+];
 
 
 
@@ -53,6 +60,13 @@ let gift = "None";
 let mutation = "None";
 if (str + dex + wil <= 30) {
   const minStat = Math.min(str, dex, wil);
+
+  const row = minStat < 5 ? 0
+                          : minStat > 8 ? 5
+                                        : minStat - 4;
+  const col = hp - 1;
+  gift = gifts[row][col];
+
   const mutationType = generate_text("mutationType");
   const mutationPart = generate_text("mutationPart");
   mutation = mutationType + ' ' + mutationPart;
