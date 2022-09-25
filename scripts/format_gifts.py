cbae8aefec3787f0e3a95eeb604a59a6e100dc71
Daniel Subic <daniel.subic@eborsystems.com>
2022-09-25 16:06:40 +0930

    Script works properly. Replaced gift table in SRD & array in generator from output of the script

--- scripts/format_gifts.py
+++ scripts/format_gifts.py
@@ -1,3 +1,7 @@
+import sys;
+import re;
+
+# Use when changing the list of Strange Gifts to format for jekyll or the generator.
 
 # Gifts in rough order from most to least good. Must be exactly 36 of them!
 
@@ -44,22 +48,69 @@ if len(gifts_list) != 36:
     print("Wrong number of gifts: " + str(len(gifts_list)))
     exit()
 
-# Ordering:
-# 01 03 06 10 15 21
-# 02 05 09 14 20 26
-# 04 08 13 19 25 30
-# 07 12 18 24 29 33
-# 11 17 23 28 32 35 
-# 16 22 27 31 34 36
-
 # Populate grid
-gift_index = 0
-output_grid = [[0 for x in range(6)] for y in range(6)] 
-for i in range(12):
-    for j in range(i + 1):
-        if (i - j > 5) | (j > 5):
-            continue
-        output_grid[i - j][j] = gifts_list[gift_index]
-        gift_index += 1
+def populate_grid():
+    # Ordering:
+    # 01 03 06 10 15 21
+    # 02 05 09 14 20 26
+    # 04 08 13 19 25 30
+    # 07 12 18 24 29 33
+    # 11 17 23 28 32 35 
+    # 16 22 27 31 34 36
 
-print(output_grid)
+    gift_index = 0
+    output_grid = [[0 for x in range(6)] for y in range(6)] 
+    for i in range(12):
+        for j in range(i + 1):
+            if (i - j > 5) | (j > 5):
+                continue
+            output_grid[i - j][j] = gifts_list[gift_index]
+            gift_index += 1
+    return output_grid
+
+def format_jekyll():
+    grid = populate_grid()
+    table_header_1 = "| Lowest Ability Score | 1 HP | 2 HP | 3 HP | 4 HP | 5 HP | 6 HP |"
+    table_header_2 = "| -------------------- | ---- | ---- | ---- | ---- | ---- | ---- |"
+
+    def format_row(row):
+        str = "|"
+        for item in row:
+            str += " " + item + " |"
+        return str
+
+    column_headers = ["3-4", "5", "6", "7", "8", "9-10"]
+
+    print(table_header_1)
+    print(table_header_2)
+    for idx, row in enumerate(grid):
+        print(column_headers[idx] + " " + format_row(row))
+
+def format_generator():
+    grid = populate_grid()
+    print("[")
+    for r_idx, row in enumerate(grid):
+        str = ' ['
+        for c_idx, cell in enumerate(row):
+            str += '\"' + cell + '\"'
+            if c_idx != len(row) - 1:
+                str += ', '
+        str += ']'
+        if r_idx != len(grid):
+            str += ','
+        print(str)
+    print("]")
+
+if len(sys.argv) == 1:
+    print('Usage:')
+    print(' -j  Format gifts for jekyll.')
+    print(' -g  Format gifts for the generator.')
+    exit()
+
+option = sys.argv[1]
+if option == '-j':
+    format_jekyll()
+elif option == '-g':
+    format_generator()
+else:
+    print('Unknown opyion: ' + sys.argv[1])
